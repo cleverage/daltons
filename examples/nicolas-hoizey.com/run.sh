3d4f0e165c42264028cf6218632b87bb09d4403b
Nicolas Hoizey <nicolas@hoizey.com>
2018-08-30 08:55:57 +0200

    Store the page localy

--- examples/nicolas-hoizey.com/run.sh
+++ examples/nicolas-hoizey.com/run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-node ../../responsive-image-sizes.js --contextsfile ./contexts.csv --url 'https://nicolas-hoizey.com/2017/01/how-much-data-should-my-service-worker-put-upfront-in-the-offline-cache.html' --selector '.main img[srcset]:first-of-type' --maxviewport 2560 --delay 100 --verbose
+path=`pwd`
+node ../../responsive-image-sizes.js --contextsfile ./contexts.csv --url "file://${path}/page.html" --selector '.main img[srcset]:first-of-type' --maxviewport 2560 --delay 100 --verbose
