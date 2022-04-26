554e31eca85d75bdfa24ccacda832cdcfcde53fd
Nicolas Hoizey <nhoizey@clever-age.com>
2022-04-26 23:59:46 +0200

    Replace k-means with a better algorithm
    Refs https://github.com/cleverage/daltons/issues/394

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const skmeans = require('skmeans')
 
 const writeFile = util.promisify(fs.writeFile)
 
+const fact = (n) => (n === 0 ? 1 : n * fact(n - 1))
+
 const defaultOptions = {
   url: null,
   selector: 'img',
@@ -79,75 +81,161 @@ module.exports = async function main(settings) {
       totalViews += value.views
     }
   })
-  // Change views numbers to percentages
-  perfectWidths.forEach((views, width) => {
-    perfectWidths.set(width, views / totalViews)
-  })
-  // sort by decreasing percentages
-  perfectWidths = new Map(
+  let numberOfPerfectWidths = perfectWidths.size
+  logger.info(
+    color.green(`${numberOfPerfectWidths} perfect widths have been computed`),
+  )
+
+  // sort by decreasing views
+  let perfectWidthsByDecreasingViews = new Map(
     [...perfectWidths.entries()].sort((a, b) => {
-      if (a[1] == b[1]) {
-        // same percentage, sort by image width
+      if (a[1] === b[1]) {
+        // same number of views, sort by image width
         return b[0] - a[0]
       }
       return b[1] - a[1]
     }),
   )
 
-  logger.info(
-    color.green(`${perfectWidths.size} perfect widths have been computed`),
-  )
-  const perfectWidthsTable = logger.newTable({
-    head: ['percentage', 'image width'],
-    colAligns: ['right', 'right'],
+  const perfectWidthsTableByViews = logger.newTable({
+    head: ['views', 'percentage', 'image width'],
+    colAligns: ['right', 'right', 'right'],
     style: {
-      head: ['green', 'green'],
+      head: ['green', 'green', 'green'],
       compact: true,
     },
   })
-  if (perfectWidthsTable) {
-    perfectWidths.forEach((percentage, imageWidth) => {
-      let roundedPercentage = percentage * 100
-      perfectWidthsTable.push([
-        roundedPercentage.toFixed(2) + ' %',
-        imageWidth + 'px',
-      ])
+  if (perfectWidthsTableByViews) {
+    perfectWidthsByDecreasingViews.forEach((views, imageWidth) => {
+      let percentage = ((views / totalViews) * 100).toFixed(2) + ' %'
+      perfectWidthsTableByViews.push([views, percentage, imageWidth + 'px'])
     })
-    logger.info(perfectWidthsTable.toString())
+    logger.info('\nPerfect widths per decreasing views:')
+    logger.info(perfectWidthsTableByViews.toString())
   }
 
-  logger.info(color.green(`Find ${options.widthsNumber} best widths`))
-
-  let closestRealWidths = []
-  // Use the k-means algorithm to find the centroids of widths distribution,
-  // with the k-means++ cluster initialization method
-  const result = skmeans(
-    [...perfectWidths.entries()],
-    options.widthsNumber,
-    'kmpp',
+  // sort by decreasing width
+  let perfectWidthsByDecreasingWidths = new Map(
+    [...perfectWidths.entries()].sort((a, b) => b[0] - a[0]),
   )
-  console.dir(result)
-  result.centroids.forEach((centroid) => {
-    const centroidWidth = centroid[0]
-    closestRealWidths.push(
-      [...perfectWidths.keys()].reduce((prev, curr) => {
-        return Math.abs(prev - centroidWidth) > Math.abs(curr - centroidWidth)
-          ? curr
-          : prev
-      }),
+
+  // Show perfect widths per decreasing widths in the console only when there are 20 or less
+  if (numberOfPerfectWidths <= 20) {
+    const perfectWidthsTableByWidths = logger.newTable({
+      head: ['image width', 'views', 'percentage'],
+      colAligns: ['right', 'right', 'right'],
+      style: {
+        head: ['green', 'green', 'green'],
+        compact: true,
+      },
+    })
+    if (perfectWidthsTableByWidths) {
+      perfectWidthsByDecreasingWidths.forEach((views, imageWidth) => {
+        let percentage = ((views / totalViews) * 100).toFixed(2) + ' %'
+        perfectWidthsTableByWidths.push([imageWidth + 'px', views, percentage])
+      })
+      logger.info('\nPerfect widths per decreasing widths:')
+      logger.info(perfectWidthsTableByWidths.toString())
+    }
+  }
+
+  let optimalWidths = []
+  if (numberOfPerfectWidths <= options.widthsNumber) {
+    // TODO: enhance this case
+    logger.info(
+      color.green(
+        `There are already less than ${options.widthsNumber} best widths, no computation necessary`,
+      ),
     )
-  })
-  closestRealWidths.sort().slice(0, options.widthsNumber)
+    optimalWidths = perfectWidthsByDecreasingViews.values()
+  } else {
+    logger.info(color.green(`Find ${options.widthsNumber} best widths`))
+
+    // We have to keep the largest width
+    // So the number of compbinations depends on less items
+    let numberOfCombinations =
+      fact(numberOfPerfectWidths - 1) /
+      fact(numberOfPerfectWidths - options.widthsNumber - 2)
+    logger.info(
+      color.green(`There are ${numberOfCombinations} possible combinations`),
+    )
+
+    // Get all possible subset combinations in an array, with minimum and maximum lengths
+    // Adapted from https://www.w3resource.com/javascript-exercises/javascript-function-exercise-21.php
+    const subset = (items, min, max) => {
+      let result_set = []
+      let result
+
+      for (var x = 0; x < Math.pow(2, items.length); x++) {
+        result = []
+        i = items.length - 1
+        do {
+          if ((x & (1 << i)) !== 0) {
+            result.push(items[i])
+          }
+        } while (i--)
+
+        if (result.length >= min && result.length <= max) {
+          result_set.push(result)
+        }
+      }
+
+      return result_set
+    }
+
+    // Keep only width values
+    let widthValues = [...perfectWidthsByDecreasingWidths].map(
+      (item) => item[0],
+    )
+    // Extract the maximum width, to keep it anyway
+    let maxWidth = widthValues.shift()
+    // Compute subset combinations of the other sizes, and add back the max width
+    let subsets = subset(widthValues, 0, options.widthsNumber - 1).map((data) =>
+      [maxWidth].concat(data.sort((a, b) => b - a)),
+    )
+
+    const globalDistance = (actualWidths, subset) => {
+      let distance = 0
+      // Adds a floor value to the subset
+      let subsetCopy = [...subset, 0]
+      // Loop through the subset
+      for (let i = 0; i < subsetCopy.length - 1; i++) {
+        // Loop each pixel width from current to next subset value
+        for (let j = subsetCopy[i]; j > subsetCopy[i + 1]; j--) {
+          // If there's such an actual width, add the distance
+          if (actualWidths.get(j)) {
+            let additionalDistance = (subsetCopy[i] - j) * actualWidths.get(j)
+            distance += additionalDistance
+          }
+        }
+      }
+      return distance
+    }
+
+    // Compute the distance for each subset and keep the best one
+    let bestSubsetDistance = -1
+    subsets.map((subset) => {
+      let distance = globalDistance(perfectWidthsByDecreasingWidths, subset)
+      if (bestSubsetDistance === -1 || distance < bestSubsetDistance) {
+        bestSubsetDistance = distance
+        optimalWidths = subset
+      }
+    })
+  }
 
   /* -------------------------- */
 
   let srcset = []
-  closestRealWidths.forEach((width) => {
+  optimalWidths.forEach((width) => {
     srcset.push(`your/image/path.ext ${width}w`)
   })
 
   if (options.verbose) {
-    console.dir(srcset)
+    console.log(
+      `Here are the best image width for the 'srcset' attribute:\n\n${srcset.join(
+        ',\n',
+      )}`,
+    )
   }
 
   // Save data into the TXT file
